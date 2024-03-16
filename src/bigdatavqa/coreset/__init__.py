f3345c209fb80e15dc2cdd91f048e7af9d8bb491
Boniface Yogendran <boniface.yogi@gmail.com>
2024-03-16 18:44:24 -0400

    refactor(coreset.py): print the coreset being used and raise error when wrong coreset method is presented (#33)

--- src/bigdatavqa/coreset/__init__.py
+++ src/bigdatavqa/coreset/__init__.py
@@ -1,8 +1,1 @@
-from ._coreset import (
-    Coreset,
-    coreset_to_graph,
-    get_coreset_vector_df,
-    get_coreset_vectors_to_evaluate,
-    get_iteration_coreset_vectors_and_weights,
-    normalize_np,
-)
+from ._coreset import Coreset
