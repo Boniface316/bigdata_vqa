40535ac288d037d8a984d4e568b0232782adfacc
boniface316 <boniface.yogi@gmail.com>
2024-03-21 06:17:08 -0400

    refactor(_postexecution.py): remove unwanted parts of postexecution.py

--- src/bigdatavqa/postexecution/_postexecution.py
+++ src/bigdatavqa/postexecution/_postexecution.py
@@ -5,47 +5,6 @@ import numpy as np
 import pandas as pd
 
 
-def get_divisive_cluster_cost(dendo, hc, centroid_coords):
-    cost_list = []
-    for parent_posn in range(len(hc)):
-        children_lst = dendo.find_children(parent_posn)
-
-        if len(children_lst) == 0:
-            continue
-        else:
-            index_vals_temp = hc[parent_posn]
-            child_1 = children_lst[0]
-            child_2 = children_lst[1]
-
-            if isinstance(index_vals_temp[0], str):
-                index_vals_temp = [ord(c) - ord("A") for c in index_vals_temp]
-                child_1 = [ord(c) - ord("A") for c in child_1]
-                child_2 = [ord(c) - ord("A") for c in child_2]
-
-            new_df = dendo.coreset_data.iloc[index_vals_temp]
-
-            child_1_str_list = [str_val for str_val in child_1]
-
-            new_df["cluster"] = 0
-            new_df.loc[new_df.name.isin(child_1_str_list), "cluster"] = 1
-
-            cost = 0
-
-            for idx, row in new_df.iterrows():
-                if row.cluster == 0:
-                    cost += (
-                        np.linalg.norm(row[["X", "Y"]] - centroid_coords[child_1]) ** 2
-                    )
-                else:
-                    cost += (
-                        np.linalg.norm(row[["X", "Y"]] - centroid_coords[child_2]) ** 2
-                    )
-
-            cost_list.append(cost)
-
-    return cost_list
-
-
 def get_distance_between_two_vectors(
     vector1: np.ndarray, vector2: np.ndarray, weight: np.ndarray
 ) -> float:
